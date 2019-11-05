2773dfacdea2cb2466b6d1c312181b984f6519c5
Kannan Periasamy <periasamy.k@HCL.COM>
2019-11-05 16:12:21 +0530

    number to roman conversion conveter developed in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ export class AppComponent {
     this.error= '';
     const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
     let a;
-    if(this.number > 3999)
-    this.error = "Enter a number between 1 and 3999";
-    else{
+   // if(this.number > 3999)
+    //this.error = "Enter a number between 1 and 3999";
+   // else{
       for(let key in romanNumList){
           a = Math.floor(this.number / romanNumList[key]);
           if(a >= 0){
@@ -27,6 +27,6 @@ export class AppComponent {
             }
           this.number = this.number % romanNumList[key];
       }
-    }  
+    //}  
   }
 }
